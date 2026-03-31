8bad90d935131dda2dc5c99eee656675f7544c8a
kanika <kanikakur14@gmail.com>
2026-03-31 11:48:16 +0530

    Added code to find 2nd higest from list

--- src/Q5.java
+++ src/Q5.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Q5 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class Q5 {
+    //Second highest element from a list
+    public static void main(String args[]){
+        List<Integer> list=List.of(1,56,45,4,77,4,3);
+        Optional<Integer> result =list.stream().sorted((a, b)->b-a).skip(1).findFirst();
+        System.out.println(result.orElse(null));
+    }
 }
